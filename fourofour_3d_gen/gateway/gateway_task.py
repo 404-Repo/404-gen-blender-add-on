8207013f86bdf4a5b9d1c1dc61463fc18d710638
Ruben Hohndorf <ruben.hohndorf@gmail.com>
2025-10-13 21:40:50 +0200

    Clean up GatewayTask class

--- fourofour_3d_gen/gateway/gateway_task.py
+++ fourofour_3d_gen/gateway/gateway_task.py
@@ -26,9 +26,5 @@ class GatewayTask(BaseModel):
     result: bytes | None = None
     status: GatewayTaskStatus = GatewayTaskStatus.NO_RESULT
     reason: str | None = None
-    prompt: str | None = None
-    image: str | None = None
-    start_time: datetime | None = None
-    obj_type: str | None = None
-    replace_obj: bpy.types.Object | None = None
+
 
